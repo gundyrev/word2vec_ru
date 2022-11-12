6f89ff9fa6e896603a8505482070f088cb77b463
gundyrev <ivan.gundyrev@yandex.ru>
2022-11-13 02:11:17 +0300

    model_testing.py update

--- model_testing.py
+++ model_testing.py
@@ -13,12 +13,24 @@ def model_info(w2v_model: KeyedVectors):
     print('Топ 20 часто используемых слов: {}'.format(', '.join(model_vocab[:20])))
 
 
+def similarity(w2v_model: KeyedVectors, word1: str, word2: str):
+    words_similarity = w2v_model.similarity(word1, word2)
+    print('Схожесть между словами "{}" и "{}" - {}'.format(word1, word2, words_similarity))
+
+
 if __name__ == "__main__":
     # parse args
     parser = argparse.ArgumentParser(description='information about word2vec model')
     parser.add_argument('model', type=str, help='path to model')
+    parser.add_argument('--info', action='store_true', help='print info about model')
+    parser.add_argument('--similarity', type=str, help='similarity between two words, separated by comma')
     args = parser.parse_args()
     # open model from file
     model = open_model(args.model)
-    # print info about model
-    model_info(model)
+    # if needed print info about model
+    if args.info:
+        model_info(model)
+    # if needed print similarity between two words
+    if args.similarity:
+        words = args.similarity.split(',')
+        similarity(model, words[0], words[1])
